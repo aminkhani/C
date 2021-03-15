9db6fc6be22c791877ddb3b6fb94d8459f45ad3a
Amin_Ai <51851950+aminkhani@users.noreply.github.com>
2021-03-15 13:13:11 +0330

    Update exercise-4.c

--- Q2/exercise-4.c
+++ Q2/exercise-4.c
@@ -2,16 +2,16 @@
 
 int main(){
 
-    int input;
-    scanf("%d", &input);
-
-
-
-
-
-
-
-
-
+    float input1, input2, input3;
+    printf("Enter the first angle of the triangle : ");
+    scanf("%f", &input1);
+    printf("Enter the secend angle of the triangle : ");
+    scanf("%f", &input2);
+    if((input1 + input2) >= 90)
+        printf("It Is Not Correct!!!\nTry Again ...\n"); 
+    else{    
+        input3 = (180 - (input1 + input2));
+        printf("the third angle of the triangle is : %.1f\n", input3);
+    }
     return 0;
 }
